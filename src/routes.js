8446b530d2505d6a2262f007d4f96610a431a5c7
kleysonc castro <kleysoncastro99@gmail.com>
2019-10-17 19:19:37 -0300

    Feat: jwt e authozizacao por token: feito

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,13 @@
 import { Router } from 'express';
 import UserController from './app/controller/UserController';
+import SessionController from './app/controller/SessionController';
+import authMidlleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMidlleware);
+routes.put('/user', UserController.update);
 export default routes;
