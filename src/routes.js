e169587c55e0d7273d0e5da3907ddcc4b02bc31c
kleysonc castro <kleysoncastro99@gmail.com>
2019-10-18 17:23:57 -0300

    feat: upload de img usando mult

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,21 @@
 import { Router } from 'express';
+import multer from 'multer';
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
 import authMidlleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMidlleware);
+
 routes.put('/user', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  res.json({ ok: true });
+});
 export default routes;
