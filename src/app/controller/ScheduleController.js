9aff88816f26601ef877825b0e3bcad1da9d37a4
kleysonc castro <kleysoncastro99@gmail.com>
2019-10-22 16:57:44 -0300

    chore: testando

---
+++ src/app/controller/ScheduleController.js
@@ -0,0 +1,35 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkIsProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ erro: 'Usuario não é um provedor de serviços' });
+    }
+
+    const { date } = req.requery;
+    const parseDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+        },
+      },
+      order: ['date'],
+    });
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
