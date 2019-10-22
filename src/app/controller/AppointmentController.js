99fa9bcd516ade0a80c0ba5d1314a675b879eb64
kleysonc castro <kleysoncastro99@gmail.com>
2019-10-21 21:06:41 -0300

    chore: avaliação de datas e formatacao delas

--- src/app/controller/AppointmentController.js
+++ src/app/controller/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -18,20 +19,49 @@ class AppointmentController {
      * Check se provider_id é um provider
      */
 
-    const IsProvider = await User.findOne({
+    const checkIsProvider = await User.findOne({
       where: { id: provider_id, provider: true },
     });
-    if (!IsProvider) {
+    if (!checkIsProvider) {
       return res.status(401).json({ erro: 'voce nao tem permição' });
+    }
+
+    /*
+     * check se data para cadastro e menor que data atual.
+     * startOfHour, pega data, e a hora inteira, ex: 18:00, 3:00
+     */
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ erro: 'Essa data nao é permitida' });
+    }
+
+    /*
+     * check se hara para agendamento esta disponivel
+     */
+
+    console.log(hourStart);
+
+    const checkHour = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_ate: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkHour) {
+      return res.status(400).json({ erro: 'Esse horario não é permitido' });
     }
 
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
 
-    res.json(appointment);
+    return res.status(200).json(appointment);
   }
 }
 
